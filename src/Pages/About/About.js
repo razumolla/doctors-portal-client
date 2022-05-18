68249fe54752b5d887ba33046fed764d5277f0b2
razumolla <rajuahmed9344@gmail.com>
2022-05-18 17:59:20 +0600

    Project setup and navbar added

---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h3>Do you who I am..???</h3>
+            <h3>I am a world Famous Programer And Problem solver.</h3>
+        </div>
+    );
+};
+
+export default About;
